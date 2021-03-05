93c546921134457225d6db45c5cc704e5102e375
jcwijaya <54810221+jcwijaya@users.noreply.github.com>
2021-03-05 09:12:19 -0600

    updated start.sh to current verison

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-sudo service rpcbind start
-sudo service nfs-kernel-server start
-sudo service nfs-common start
-sudo exportfs -a 
-sudo mount -t nfs -o v3,proto=tcp,port=2049 172.17.0.2:/mnt/example /home/jovyan/example
+service rpcbind start
+service nfs-kernel-server start
+service nfs-common start
+exportfs -a 
+sleep infinity
